1c959b978285bb4818b2b24b4c01389e321ab358
masudpro121 <masud.pro.121@gmail.com>
2023-08-30 20:53:19 +0600

    our generated system done

--- src/components/ShowGeneratedImage/ShowGeneratedImages.js
+++ src/components/ShowGeneratedImage/ShowGeneratedImages.js
@@ -1,39 +1,45 @@
-import React from 'react'
-import RenderImage from "../RenderImage/RenderImage"
+import React from "react";
+import RenderImage from "../RenderImage/RenderImage";
 
-function ShowGeneratedImages({progress, progressImage, generatedImages}) {
+function ShowGeneratedImages({ progress, progressImage, generatedImages }) {
   return (
     <div>
-      {
-        progress == 100 ? 
+      {progress == 100 ? (
         <div>
-          <div>Progress Done</div>
-          {
-            generatedImages.map((img, i)=> {
+          <div className="text-center mt-20 text-3xl font-bold text-gray-700">
+            Our Generated Images
+          </div>
+          <div className="flex flex-wrap gap-5 m-auto justify-center my-5">
+            {generatedImages.map((img, i) => {
               return (
-                <div>
-                  <RenderImage src={img} key={img+i} />
+                <div className="">
+                  <RenderImage src={img} key={img + i} />
                 </div>
-              )
-            })
-          }
-        </div>
-        : progress > 0 ?
-        <div> 
-          <div>Inprogress with Image: {progress}</div>
-          <div>
-            <RenderImage src={progressImage} />
+              );
+            })}
           </div>
         </div>
-        :<div>
-          <div>Inprogress {progress}</div>
-          <div>
-            <RenderImage />
+      ) : progress > 0 ? (
+        <div>
+          <div className="text-center mt-20 text-3xl font-bold text-gray-700">
+            Progress {progress}%
+          </div>
+          <div className="m-auto flex flex-wrap justify-center my-10">
+            <RenderImage src={progressImage} cls="h-[400px] w-[400px]" />
           </div>
         </div>
-      }
+      ) : (
+        <div>
+          <div className="text-center mt-20 text-3xl font-bold text-gray-700">
+            Progress {progress}%
+          </div>
+          <div className="m-auto flex flex-wrap justify-center my-10">
+            <RenderImage cls="h-[400px] w-[400px]" />
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default ShowGeneratedImages
+export default ShowGeneratedImages;
