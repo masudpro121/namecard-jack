fc379e430957e8fd5c7f9db29ce4b1811d30f7fb
masudpro121 <masud.pro.121@gmail.com>
2023-09-22 07:23:43 +0600

    book recommendation done

---
+++ src/assets/vocabulary/recommended-books.js
@@ -0,0 +1,27 @@
+import BookImg from "./images/book.jpg"
+import HighlightBook from "./images/highlight.jpg"
+
+export default [
+  {
+    img: HighlightBook
+  },
+  {
+    img: BookImg
+  },
+  {
+    img: BookImg
+  },
+  {
+    img: BookImg
+  },
+  {
+    img: BookImg
+  },
+  {
+    img: BookImg
+  },
+  {
+    img: BookImg
+  }
+  
+]
--- src/pages/vocabulary.js
+++ src/pages/vocabulary.js
@@ -1,10 +1,14 @@
+import recommendedBooks from "@/assets/vocabulary/recommended-books"
 import vocabularyCategory from "@/assets/vocabulary/vocabulary-category"
 import Image from "next/image"
 import React, { useState } from 'react'
 
 const Vocabulary = () => {
-  const parentCategory = Object.keys(vocabularyCategory)
   const [word, setWord] = useState('')
+  const parentCategory = Object.keys(vocabularyCategory)
+  const highlightedBook = recommendedBooks[0]
+  console.log(highlightedBook);
+  const recommends = recommendedBooks.slice(1)
   return (
     <div>
       {/* Search Vocabulary  */}
@@ -20,7 +24,7 @@ const Vocabulary = () => {
         parentCategory.map((category, i)=>{
           if(vocabularyCategory[category].length>0){
             return (
-              <div key={category+i} className="mt-20 bg-gray-300 py-5 px-5  m-5 rounded-md">
+              <div key={category+i} className="mt-20 bg-gray-200 py-5 px-5  m-5 rounded-md">
                 <div className="mb-5 text-2xl font-bold text-slate-900">{category}</div>
                 <div className="flex flex-wrap gap-x-5 gap-y-10">
                 {
@@ -29,7 +33,7 @@ const Vocabulary = () => {
                       <div className="sm:w-[48%] md:w-[30%] " key={"categoryItem"+key}>
                         <div>
                           <Image src={item.img} alt="" className="w-full rounded-tl-md rounded-tr-md" />
-                          <div className="bg-slate-900 text-white py-3 px-4 font-semibold rounded-bl-md rounded-br-md">
+                          <div className="bg-slate-800 text-white py-3 px-4 font-semibold rounded-bl-md rounded-br-md">
                             {item.category}
                           </div>
                         </div>
@@ -44,6 +48,28 @@ const Vocabulary = () => {
            
         })
       }
+
+
+      {/* Recommendation  */}
+      <div className=" bg-slate-800 m-5 p-10">
+        <div>
+          <h1 className="text-4xl font-bold text-white">We <span className=" text-orange-500">Recommend</span> these Books</h1>
+        </div>
+        <div className="block md:flex gap-5 mt-9 justify-between">
+          <div className="w-full md:w-[30%]">
+            <Image src={highlightedBook.img} className=" w-full" />
+          </div>
+          <div className=" mt-5 md:mt-0 flex flex-wrap justify-center md:justify-end gap-5 md:w-[70%]">
+            {
+              recommends.map((book, i)=>{
+                return (
+                  <Image key={"book"+i} src={book.img} className="w-full sm:w-[48%] md:w-[30%]" />
+                )
+              })
+            }
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
